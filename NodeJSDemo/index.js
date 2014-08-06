2e714c792944f7990e32cf198c7fedaa415cde71
donegal <murdercdh@hotmail.com>
2014-08-06 14:43:50 +0800

    Revert "re update the framework with demo"
    This reverts commit b70cee8b33e2eac2aef78d1dc09bc861350c607e.

---
+++ NodeJSDemo/index.js
@@ -0,0 +1,48 @@
+﻿var express = require( 'express' );
+var mysql = require( 'mysql' );
+
+var pool = mysql.createPool( {
+    connectionLimit: 10,
+    host: '115.28.205.176',
+    user: 'xxxx',
+    password: 'xxxxxx'
+});
+
+var app = express();
+
+app.get( '/', function ( req, res ) {
+    res.send( "please choose controller to get data! like/hotword" );
+});
+
+app.get( '/hotword', function ( req, res ) {
+    pool.getConnection( function ( err, connection ) {
+        connection.query( 'SELECT * from digital_marketing.r_brand_hotword', function ( err, rows, fields ) {
+            if ( err ) {
+                throw err;
+            }
+            debugger;
+            connection.release();
+            return res.send( rows );
+        });
+    });
+    //connection.end();
+});
+
+function GethotWord() {
+    connection.connect();
+    var result;
+    connection.query( 'SELECT * from digital_marketing.r_brand_hotword', function ( err, rows, fields ) {
+        if ( err ) {
+            throw err;
+        }
+        debugger;
+        result = rows;
+    });
+
+    connection.end();
+    return result;
+}
+
+app.listen( 1337 );
+
+console.log( 'Express app started on port 1337' );
--- seajs_animate/README.md
+++
@@ -1,4 +0,0 @@
-seajs_animate
-=============
-
-This is a lightweight animate libary base on seajs.It support animation effect of DOM element and custom object. tweened  function is also supported
